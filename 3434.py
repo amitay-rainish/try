8966bad7a6e57ac253fcf97d63e644619284f595
amitay-rainish <amitay07044@walla.com>
2017-03-30 13:52:21 +0300

    stam

---
+++ 3434.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    """
+    Add Documentation here
+    """
+    pass  # Replace Pass with Your Codek
+print 1
+if True:
+    print 2
+print 3
+Thread(target=2).start()
+
+if __name__ == '__main__':
+    main()
